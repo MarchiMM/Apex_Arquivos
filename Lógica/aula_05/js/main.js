937d1234beae6b79425d1494077dc1fabae8348b
Matheus Marchi Moro <neninhomat@gmail.com>
2020-09-30 19:13:30 -0300

    Aulas movidas para pasta "Lógica"

---
+++ Lógica/aula_05/js/main.js
@@ -0,0 +1,6 @@
+let palavra1 = window.prompt('Informe a primeira palavra')
+let palavra2 = window.prompt('Informe a segunda palavra')
+let palavra3 = window.prompt('Informe a terceira palavra')
+let palavra4 = window.prompt('Informe a quarta palavra')
+
+document.write(`<p>${palavra1} - ${palavra2} - ${palavra3} - ${palavra4}</p>`)
