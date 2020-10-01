717fe792624babeea48cc689871f273ffa705b38
Matheus Marchi Moro <neninhomat@gmail.com>
2020-09-30 22:23:02 -0300

    aula_05

---
+++ Lógica/Tarefas/js/exercicio_calculadora.js
@@ -0,0 +1,24 @@
+/*
+    Calculadora
+        operacao pode ser + ou - ou * ou /
+
+    Crie as seguintes funçoes:
+        somar(numero1, numero2) : number
+        subtrair(numero1, numero2) : number
+        multiplicar(numero1, numero2) : number
+        dividir(numero1, numero2) : number
+        
+        efetuarCalculo(numero1, numero2, operacao) : number
+            dependendo da operacao, ira efetuar a chamada para a função que faz o calculo solicitado
+            nesta função utilizar o switch
+
+        OBS: Lembrem que quem retorna o resultado neste exercicio é a funcao efetuarCalculo
+
+    solicitar ao usuario o primeiro numero - parseFloat()
+    solicitar ao usuario o segundo numero - parseFloat()
+    solicitar ao usuario a operacao -  é uma string ("+", "-", "*", "/")
+
+    exibir na tela o resultado do calculo conforme solicitado pelo usuario
+
+    OBRIGATÓRIO: utilizar apenas uma vez o document.write()
+*/
