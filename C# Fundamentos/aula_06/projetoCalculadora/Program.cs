528cc75f392588b1bec187d64cfd49e526de594a
Matheus Marchi Moro <neninhomat@gmail.com>
2020-10-27 20:16:07 -0300

    Tarefa aula_06 feita

---
+++ C# Fundamentos/aula_06/projetoCalculadora/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using projetoCalculadora.Servicos;
+
+namespace projetoCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o primeiro número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe o segundo número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe a operação (+, -, *, /): ");
+            string operacao = Console.ReadLine();
+
+            Calculadora objCalculadora = new Calculadora(n1, n2, operacao);
+
+            Console.WriteLine($"O resultado é: {objCalculadora.EfetuarCalculo()}");
+        }
+    }
+}
---
+++ C# Fundamentos/aula_06/projetoCalculadora/Servicos/Calculadora.cs
@@ -0,0 +1,33 @@
+using projetoCalculadora.Servicos;
+
+namespace projetoCalculadora.Servicos
+{
+    public class Calculadora
+    {
+        private double Numero1 { get; set; }
+        private double Numero2 { get; set; }
+        private string Operacao { get; set; }
+
+        public Calculadora(double numero1, double numero2, string operacao)
+        {
+            this.Numero1 = numero1;
+            this.Numero2 = numero2;
+            this.Operacao = operacao;
+        }
+
+        public double EfetuarCalculo()
+        {
+            CalculoServico objCalculo = new CalculoServico(this.Numero1, this.Numero2);
+            double resultado = 0;
+            switch (this.Operacao)
+            {
+                case "+": resultado = objCalculo.Somar(); break;
+                case "-": resultado = objCalculo.Subtrair(); break;
+                case "*": resultado = objCalculo.Multiplicar(); break;
+                case "/": resultado = objCalculo.Dividir(); break;
+                default: break;
+            }
+            return resultado;
+        }
+    }
+}
