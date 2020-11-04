7c92a10dbe413675c92fcde5edf691688b734fc4
Matheus Marchi Moro <neninhomat@gmail.com>
2020-11-04 13:55:36 -0300

    Exercícios feitos

--- C# Fundamentos/aula_07/Exercicios/projetoImpostoCategoriaProduto/Program.cs
+++ C# Fundamentos/aula_07/Exercicios/projetoImpostoCategoriaProduto/Program.cs
@@ -6,7 +6,13 @@ namespace projetoImpostoCategoriaProduto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Produto objProduto1 = new Produto("Headset", "Eletrônico", 110);
+            Produto objProduto2 = new Produto("Camiseta", "Vestuário", 35.90);
+            Produto objProduto3 = new Produto("Caneta", "Material Escolar", 1.75);
+
+            Console.WriteLine(objProduto1.ObterMensagemDoProduto());
+            Console.WriteLine(objProduto2.ObterMensagemDoProduto());
+            Console.WriteLine(objProduto3.ObterMensagemDoProduto());
         }
     }
 }
