442aa345079341441fd3a3b93b984a4f09871ad6
sarahneuburger <neninhomat@gmail.com>
2020-11-09 22:25:36 -0300

    ASPNETCORE aula_01

---
+++ ASPNETCORE/projAspNetCore/projAspNetCore/Contexto/ContextoDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projAspNetCore.Models;
+
+namespace projAspNetCore.Contexto
+{
+    public class ContextoDB : DbContext
+    {
+        public ContextoDB(DbContextOptions<ContextoDB> opcoes) : base(opcoes)
+        {
+
+        }
+
+        public DbSet<Funcionario> Funcionario { get; set; }
+    }
+}
