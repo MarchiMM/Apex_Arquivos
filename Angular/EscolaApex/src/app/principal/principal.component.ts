2ca1c8663b3d76b3594d6160465143108c67344b
MarchiMM <71672437+MarchiMM@users.noreply.github.com>
2020-11-23 22:03:53 -0300

    Angular aula_02

--- Angular/EscolaApex/src/app/principal/principal.component.ts
+++ Angular/EscolaApex/src/app/principal/principal.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponent implements OnInit {
 
+  public tituloPrincipal = 'Principal';
+
   constructor() { }
 
   ngOnInit(): void {
