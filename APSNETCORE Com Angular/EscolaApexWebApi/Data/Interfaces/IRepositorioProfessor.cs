8f0a7fac4ecee528ed62acd3be0e8bf4db53e367
Matheus Marchi Moro <neninhomat@gmail.com>
2020-12-02 16:18:32 -0300

    Métodos professor

--- APSNETCORE Com Angular/EscolaApexWebApi/Data/Interfaces/IRepositorioProfessor.cs
+++ APSNETCORE Com Angular/EscolaApexWebApi/Data/Interfaces/IRepositorioProfessor.cs
@@ -6,7 +6,7 @@ namespace EscolaApexWebApi.Data.Interfaces
     public interface IRepositorioProfessor
     {
         Task<Professor[]> ObterTodosAsync(bool incluirAluno);
+        Task<Professor> ObterProfessorPeloIdAsync(int professorId, bool incluirAluno);
         Task<Professor[]> ObterTodosPeloAlunoIdAsync(int alunoId, bool incluirDisciplina);
-        Task<Professor[]> ObterPeloId(int professorId, bool incluirAluno);
     }
 }
